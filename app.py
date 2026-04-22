090928de4fea0d7a06a600bc2f898fa0270a57ba
deborah-andrade846 <deborahandrade846@gmail.com>
2026-04-22 16:48:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -347,7 +347,6 @@ if dados:
                     border: 1px solid rgba(255, 255, 255, 0.2);
                     border-radius: 24px;
                     padding: 40px 400px;
-                    width: 100%;
                     box-shadow: 0 15px 35px rgba(0,0,0,0.2);
                     animation: fadeInUp 0.8s ease both;
                     align-items: center;
